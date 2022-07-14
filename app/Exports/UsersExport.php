15a3e19c2fc335199b2efb74dc1b8f6e807982cc
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-07-14 13:30:37 +0700

    Membuat Export untuk user Export

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -3,10 +3,16 @@
 namespace App\Exports;
 
 use App\Models\User;
+use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection, Responsable
 {
+    use Exportable;
+
+    private $fileName = "users.xlsx";
+    
     /**
     * @return \Illuminate\Support\Collection
     */
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserExportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/export', [UserExportController::class, 'export']);
