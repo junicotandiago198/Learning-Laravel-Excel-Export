e82ba9fa2a8726751893eb03088c719582cd4ef1
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-07-14 13:44:47 +0700

    Install Package and use excel dompdf

--- app/Http/Controllers/UserExportController.php
+++ app/Http/Controllers/UserExportController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Exports\UsersExport;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Excel;
 
 class UserExportController extends Controller
 {
+    private $excel;
+
+    public function __construct(Excel $excel)
+    {
+        $this->excel = $excel;
+    }
+
     public function export()
     {
-        return new UsersExport;
+        return $this->excel->download(new UsersExport, 'users.pdf', Excel::DOMPDF);
     }
 }
