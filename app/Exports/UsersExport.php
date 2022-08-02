5f61cfb04709d9418f99e9da636c071ffbfea515
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-08-02 08:55:06 +0700

    add fitur autosize,logo dll

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -3,33 +3,89 @@
 namespace App\Exports;
 
 use App\Models\User;
-use Illuminate\Contracts\Support\Responsable;
 use Illuminate\View\View;
-use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
-class UsersExport implements FromCollection, ShouldAutoSize, WithMapping
+class UsersExport implements 
+    ShouldAutoSize, 
+    WithMapping, 
+    WithHeadings,
+    WithEvents,
+    FromQuery,
+    WithDrawings,
+    WithCustomStartCell
 {
     use Exportable;
     
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function query()
     {
-        return User::with('address')->get();
+        return User::query()->with('address');
     }
 
-    public function map($user): array
+    public function map($sdjumlahsiswa): array
     {
         return [
-            $user->id,
-            $user->email,
-            $user->address->country,
-            $user->created_at
+            $sdjumlahsiswa->id,
+            $sdjumlahsiswa->email,
+            $sdjumlahsiswa->address->country,
+            $sdjumlahsiswa->created_at
         ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'Email',
+            'Country',
+            'Created At'
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event)
+            {
+                $event->sheet->getStyle('A8:D8')->applyFromArray([
+                    'font' => [
+                        'bold' => true
+                    ],
+
+                ]);
+            }
+        ];
+    }
+
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('Logo');
+        $drawing->setDescription('This is my logo');
+        $drawing->setPath(public_path('/laravel-logo.png'));
+        $drawing->setHeight(90);
+        $drawing->setCoordinates('B3');
+
+        return $drawing;
+    }
+
+    public function startCell(): string
+    {
+        return 'A8';
     }
 }
